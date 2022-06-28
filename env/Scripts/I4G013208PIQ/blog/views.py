e7482ad8d74f5831433f9ffae5d1336ca36c6d8d
unknown <gistblize@gmail.com>
2022-06-28 18:56:09 +0100

    third time

--- env/Scripts/I4G013208PIQ/blog/views.py
+++ env/Scripts/I4G013208PIQ/blog/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render
-from .models import blog
-from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Post, blog
+from django.views.generic.edit import CreateView,UpdateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
+
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
+    
